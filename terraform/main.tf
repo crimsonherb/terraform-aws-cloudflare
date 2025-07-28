8f3d32bba14d85767ebb421c3e310b1bb06e4843
Joma Villanueva <villanueva.joma@gmail.com>
2025-07-28 23:28:04 +0800

    update certificate zone

--- terraform/main.tf
+++ terraform/main.tf
@@ -101,6 +101,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 }
 
 resource "aws_acm_certificate" "site_cert" {
+  provider          = aws.us_east_1
   domain_name       = var.site_domain
   validation_method = "DNS"
 
